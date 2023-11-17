b5aa62cdf751f0b74a528b3ea7ad672883d769a2
Bernard Wilson <bnardy06@gmail.com>
2023-11-17 10:54:23 -0600

    changed navbar logo and added audio file

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+    
+
+
     //Make the get route work and render the index.ejs file.
     res.render("index.ejs");
+    
   });
 
   app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
+
+
 
   
