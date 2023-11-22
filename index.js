dca23783d8b9a4fef6ff898b5c82237df60863b3
Bernard Wilson <bnardy06@gmail.com>
2023-11-22 14:32:31 -0600

    working on quote of the day

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.get("/", (req, res) => {
     console.log(`Listening on port ${port}`);
   });
 
+var quotes = [
+    "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+    "The way to get started is to quit talking and begin doing. -Walt Disney",
+    "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma which is living with the results of other people's thinking. -Steve Jobs",
+    "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
+    "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
+    "Be yourself; everyone else is already taken. -Oscar Wilde"
+  ]
+  function newQuote() {
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+  }
 
   
  
